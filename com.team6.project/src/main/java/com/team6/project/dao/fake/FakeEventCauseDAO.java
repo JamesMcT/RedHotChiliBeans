d844f5b24c7f79903e12e678c4c8793f667adb65
Eoin Kernan <eoinkernan@gmail.com>
2015-02-16 11:58:43 +0000

    Added Sabee's Fake DAO implementations from the shared google drive folder.

---
+++ com.team6.project/src/main/java/com/team6/project/dao/fake/FakeEventCauseDAO.java
@@ -0,0 +1,72 @@
+package com.team6.project.dao.fake;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+import com.team6.project.dao.EventCauseDAO;
+import com.team6.project.entities.EventCause;
+import com.team6.project.entities.EventCausePK;
+
+
+
+@Stateless
+@Local
+public class FakeEventCauseDAO implements EventCauseDAO{
+
+	private static  Map<Integer, EventCause> events = new HashMap<Integer, EventCause>();
+    static {
+    	events.put(961, new EventCause(0, 0, "RCC CONN SETUP - SUCCESS"));
+    	events.put(962, new EventCause(0, 1, "RCC CONN SETUP - REJECT DUE TO OVERLOAD"));
+    	events.put(992, new EventCause(1, 0, "INITIAL CTXT SETUP - SUCCESS"));
+    	events.put(993, new EventCause(1, 1, "INITIAL CTXT SETUP - ONGOING HANDOVER"));
+    }
+	
+	
+	@Override
+	public Collection<EventCause> getAllEventCauses() {		
+		return events.values();
+	}
+
+	@Override
+	public void addNewEventCauseDataSet(EventCause eventCause) {
+		EventCausePK ocPK = new EventCausePK(eventCause.getCauseCode(), eventCause.getEventId());
+		
+		events.put(ocPK.hashCode(), eventCause);		
+	}
+
+	@Override
+	public void updateEventCause(EventCause eventCause) {
+		EventCausePK ocPK = new EventCausePK(eventCause.getCauseCode(), eventCause.getEventId());
+		
+		events.put(ocPK.hashCode(), eventCause);
+	}
+
+	@Override
+	public EventCause findByCauseCodeAndEventId(Integer causeCode, Integer eventId) {
+		EventCausePK ocPK = new EventCausePK(causeCode, eventId);
+		
+		return events.get(ocPK);
+	}
+
+	@Override
+	public void deleteByCauseCodeAndEventId(Integer causeCode, Integer eventId) {
+		EventCausePK ocPK = new EventCausePK(causeCode, eventId);
+		events.remove(ocPK);
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		events.clear();
+		
+	}
+
+	
+	
+	
+	
+}
--- com.team6.project/src/main/java/com/team6/project/dao/fake/FakeOperatorCountryDAO.java
+++ com.team6.project/src/main/java/com/team6/project/dao/fake/FakeOperatorCountryDAO.java
@@ -1,49 +1,75 @@
 package com.team6.project.dao.fake;
 
+/**
+ * @author Sabee
+ * 
+ */
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
 
 import com.team6.project.dao.OperatorCountryDAO;
 import com.team6.project.entities.OperatorCountry;
+import com.team6.project.entities.OperatorCountryPK;
 
-
+@Stateless
+@Local
 public class FakeOperatorCountryDAO implements OperatorCountryDAO{
 
+	
+	private static  Map<Integer, OperatorCountry> operators = new HashMap<Integer, OperatorCountry>();
+    static {
+    	operators.put(961, new OperatorCountry(0, 0, "Ireland", "Vodafone"));
+    	operators.put(962, new OperatorCountry(0, 1, "Ireland", "Meteor"));
+    	operators.put(992, new OperatorCountry(1, 0, "UK", "Vodafone"));
+    	operators.put(993, new OperatorCountry(1, 1, "UK", "Orange"));
+    }
+	
+	
 	@Override
-	public Collection<OperatorCountry> getOperatorCountryRecords() {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<OperatorCountry> getOperatorCountryRecords() {		
+		return operators.values();
 	}
 
 	@Override
-	public void addNewOperatorCountryDataSet(OperatorCountry operatorcountry) {
-		// TODO Auto-generated method stub
+	public void addNewOperatorCountryDataSet(OperatorCountry operatorCountry) {
 		
+		OperatorCountryPK ocPK = new OperatorCountryPK(operatorCountry.getMcc(), operatorCountry.getMnc());
+				
+		operators.put(ocPK.hashCode(), operatorCountry);		
 	}
 
 	@Override
-	public void updateOperatorCountry(OperatorCountry operatorcountry) {
-		// TODO Auto-generated method stub
+	public void updateOperatorCountry(OperatorCountry operatorCountry) {
+		OperatorCountryPK ocPK = new OperatorCountryPK(operatorCountry.getMcc(), operatorCountry.getMnc());
 		
+		operators.put(ocPK.hashCode(), operatorCountry);	
 	}
 
 	@Override
-	public void findByMcc(int mcc) {
-		// TODO Auto-generated method stub
+	public OperatorCountry findByOperatorCountry(Integer mcc, Integer mnc) {
+		OperatorCountryPK ocPK = new OperatorCountryPK(mcc, mnc);
 		
+		return operators.get(ocPK.hashCode());
 	}
 
 	@Override
-	public void deleteByMccAndMnc(int mcc, int mnc) {
-		// TODO Auto-generated method stub
+	public void deleteByMccAndMnc(Integer mcc, Integer mnc) {
+		OperatorCountryPK ocPK = new OperatorCountryPK(mcc, mnc);
 		
+		operators.remove(ocPK.hashCode());		
 	}
 
 	@Override
 	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
+		operators.clear();		
 	}
+
+	
 
 	
 	
