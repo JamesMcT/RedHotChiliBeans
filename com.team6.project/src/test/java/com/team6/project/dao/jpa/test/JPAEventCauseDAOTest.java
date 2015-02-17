189ecfbf4fb34fb2234111e60f038fb0b0f027e9
Cris13 <cristianaconti13@gmail.com>
2015-02-17 14:33:05 +0000

    arquillian test

---
+++ com.team6.project/src/test/java/com/team6/project/dao/jpa/test/JPAEventCauseDAOTest.java
@@ -0,0 +1,23 @@
+package com.team6.project.dao.jpa.test;
+
+import static org.junit.Assert.fail;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Arquillian.class)
+public class JPAEventCauseDAOTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
