06a5405ff7e752cbbeba9e16e42d485fa3ca7004
Cris13 <cristianaconti13@gmail.com>
2015-03-01 16:09:30 +0000

    refactor of the rest tests for a common @Deployment and @Before

--- com.team6.project/src/test/java/com/team6/project/services/rest/test/AuthenticationTest.java
+++ com.team6.project/src/test/java/com/team6/project/services/rest/test/AuthenticationTest.java
@@ -33,83 +33,27 @@ import com.team6.project.dao.UserDAO;
 import com.team6.project.entities.User;
 
 @RunWith(Arquillian.class)
-public class AuthenticationTest {
+public class AuthenticationTest extends RestTest{
 
-    private final static String ARCHIVE_NAME = "test";
-    private final static String WEBAPP_SRC = "src/main/webapp/protected";
+    
+   
 
-    @Deployment
-    public static Archive<?> createDeployment() {
-        WebArchive a = ShrinkWrap
-                .create(WebArchive.class, ARCHIVE_NAME + ".war")
-                .addPackages(true, "com.team6.project.services",
-                             "com.team6.project.services.rest",
-                             "com.team6.project.entities",
-                             "com.team6.project.readers",
-                             "com.team6.project.dao",
-                             "com.team6.project.dao.jpa",
-                             "com.team6.project.validators")
-                .addAsResource("test-persistence.xml",
-                               "META-INF/persistence.xml")
-                .merge(ShrinkWrap.create(GenericArchive.class)
-                               .as(ExplodedImporter.class)
-                               .importDirectory(WEBAPP_SRC)
-                               .as(GenericArchive.class), "protected",
-                       Filters.includeAll())
-                .setWebXML(new File("src/main/webapp/WEB-INF/web.xml"))
-                .addAsWebInfResource("test-jboss-web.xml", "jboss-web.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE,
-                                     ArchivePaths.create("beans.xml"));
-        File[] files = Maven.resolver()
-                .resolve("com.jayway.restassured:rest-assured:2.4.0")
-                .withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-
-        files = Maven.resolver().resolve("org.apache.poi:poi:3.11")
-                .withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-
-        files = Maven.resolver().resolve("org.apache.commons:commons-io:1.3.2")
-                .withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-
-        files = Maven.resolver()
-                .resolve("commons-logging:commons-logging:1.1.3")
-                .withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-
-        return a;
-    }
-
-    @EJB
-    private UserDAO userDao;
-
-    private FormAuthConfig fac = new FormAuthConfig(
-                                                    "protected/j_security_check",
-                                                    "j_username", "j_password");
-
+    private FormAuthConfig fac;
+    private SessionFilter sessionFilter;
+    
     @Before
-    public void setup() {
-        RestAssured.config = config()
-                .logConfig(new LogConfig(System.out, true));
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.basePath = ARCHIVE_NAME;
-        RestAssured.port = 18080;
-
-        User testuser = new User();
-        testuser.setUserId("testuser");
-        testuser.setPassword("testpassword");
-        testuser.setRole("administrator");
-        userDao.addUser(testuser);
+    public void setUp(){
+        super.setUp();
+        createUsers();
+        fac = getformAuthConfig();
+        sessionFilter = new SessionFilter();
+        given().filter(sessionFilter).when().get("protected/admin").then()
+                .statusCode(200);
     }
 
     @Test
     public void test_login_success() {
-        SessionFilter sessionFilter = new SessionFilter();
-        given().filter(sessionFilter).when().get("protected/admin").then()
-                .statusCode(200);
-
-        given().auth().form("testuser", "testpassword", fac)
+        given().auth().form("admin", "admin", fac)
                 .filter(sessionFilter).when().get("protected/admin").then()
                 .body(containsString("Welcome"));
 
@@ -117,10 +61,7 @@ public class AuthenticationTest {
 
     @Test
     public void test_login_fail() {
-        SessionFilter sessionFilter = new SessionFilter();
-        given().filter(sessionFilter).when().get("protected/admin").then()
-                .statusCode(200);
-
+        
         given().auth().form("user", "password", fac).filter(sessionFilter)
                 .when().get("protected/admin").then()
                 .body(containsString("<title>Login Form</title>"));
--- com.team6.project/src/test/java/com/team6/project/services/rest/test/UserEquipmentRestTest.java
+++ com.team6.project/src/test/java/com/team6/project/services/rest/test/UserEquipmentRestTest.java
@@ -1,108 +1,48 @@
-/*package com.team6.project.services.rest.test;
+package com.team6.project.services.rest.test;
 
-import static com.jayway.restassured.RestAssured.config;
 import static com.jayway.restassured.RestAssured.given;
 
-import java.io.File;
 import java.net.URI;
 
-import javax.ejb.EJB;
 import javax.ws.rs.core.UriBuilder;
 
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ArchivePaths;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.config.LogConfig;
+import com.jayway.restassured.authentication.FormAuthConfig;
+import com.jayway.restassured.filter.session.SessionFilter;
 import com.jayway.restassured.http.ContentType;
-import com.team6.project.dao.UserDAO;
-
 
 @RunWith(Arquillian.class)
-public class UserEquipmentRestTest {
+public class UserEquipmentRestTest extends RestTest {
 
-	private final static String ARCHIVE_NAME = "test";
-	private final String baseURL = "http://localhost:8080/" + ARCHIVE_NAME;
-	 private final static String WEBAPP_SRC = "src/main/webapp/protected";
-	
-	public UserEquipmentRestTest() {}
-	
-	@Deployment
-    public static Archive<?> createDeployment() {
-		WebArchive a = ShrinkWrap
-                .create(WebArchive.class, ARCHIVE_NAME+".war")
-                .addPackages(true, 	"com.team6.project.services",
-                					"com.team6.project.services.rest",
-                					"com.team6.project.entities", 
-                					"com.team6.project.readers", 
-                					"com.team6.project.dao",
-                					"com.team6.project.dao.jpa",
-                					"com.team6.project.validators")
-                .addAsResource("META-INF/persistence.xml")
-                .addAsWebResource(new File(WEBAPP_SRC, "login.html"),
-                                   ArchivePaths.create("protected/login.html"))
-                .addAsWebResource(new File(WEBAPP_SRC, "index.html"),
-                                                     ArchivePaths.create("protected/index.html"))
-                                   .addAsWebResource(new File(WEBAPP_SRC, "error.html"),
-                                                     ArchivePaths.create("protected/error.html"))
-                                   .setWebXML(new File("src/main/webapp/WEB-INF/web.xml"))
-                                   .setWebXML(new File("src/main/webapp/WEB-INF/jboss-web.xml"))
-                .addAsWebInfResource(EmptyAsset.INSTANCE,ArchivePaths.create("beans.xml"));
-		
-		File[] files = Maven.resolver().resolve("com.jayway.restassured:rest-assured:2.4.0").withTransitivity().as(File.class);
-		//files = removeGroovyXml(files);
-        a.addAsLibraries(files);
+    private FormAuthConfig fac;
+    private SessionFilter sessionFilter;
 
-        files = Maven.resolver().resolve("org.apache.poi:poi:3.11").withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-        
-        files = Maven.resolver().resolve("org.apache.commons:commons-io:1.3.2").withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-        
-        files = Maven.resolver().resolve("commons-logging:commons-logging:1.1.3").withTransitivity().as(File.class);
-        a.addAsLibraries(files);
-        
-        return a;
+    @Before
+    public void setUp() {
+        super.setUp();
+        createUsers();
+        fac = getformAuthConfig();
+        sessionFilter = getSession();
     }
-	@EJB
-	UserDAO userDao;
 
-	*//**
-	 * This test will simply make sure the RESTfulo endpoint can be reached and that it is returning JSON
-	 * even if there are no records in the database.
-	 * 
-	 *//*
-	@Test
-	public void testGetAll(){
-		 
-		RestAssured.config = config().logConfig(new LogConfig(System.out, true));
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
-		System.err.println(userDao.getUserByKey("cristiana"));
-		
-		given().queryParam("j_username","cristiana").queryParam("j_password", "password")
-		.expect().statusCode(200)
-		 		.contentType(ContentType.JSON)
-		 		.when()
-		 		.post(buildUri("protected","rest", "userequipment", "all")).then().log().all();
-		
-		
-	 }
+    /**
+     * This test will simply make sure the RESTfulo endpoint can be reached and
+     * that it is returning JSON even if there are no records in the database.
+     * 
+     */
+    @Test
+    public void testGetAll() {
 
-	 private URI buildUri(String... paths) {
-	        UriBuilder builder = UriBuilder.fromUri(baseURL);
-	        for (String path : paths) {
-	            builder.path(path);
-	        }
-	        return builder.build();
-	 }
-	 
-}*/
+        given().auth().form("admin", "admin", fac).filter(sessionFilter)
+                .expect().statusCode(200).contentType(ContentType.JSON).when()
+                .get("/rest/userequipment/all")
+                .then().log().all();
+
+      
+    }
+
+}
