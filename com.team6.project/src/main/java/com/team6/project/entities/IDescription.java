4eabd852f1a1e057d59faa202440f897c130f5d2
johnok <neoslov@yahoo.com>
2015-03-04 03:23:18 +0000

    fixed untracked files

--- com.team6.project/src/main/java/com/team6/project/entities/IDescription.java
+++
@@ -1,5 +0,0 @@
-package com.team6.project.entities;
-
-public interface IDescription {
-    public void setDescription(String string);
-}
--- com.team6.project/src/main/webapp/js/hello.js
+++
@@ -1,6 +0,0 @@
-function Hello($scope, $http) {
-    $http.get('http://localhost:8080/PeopleEnterpriseExample-0.0.1-SNAPSHOT/rost/getall').
-        success(function(data) {
-            $scope.ErroneousBaseData = data;
-        });
-}
