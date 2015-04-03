291adcc73b949f1484639fdbf47f2867f806e041
Cris13 <cristianaconti13@gmail.com>
2015-04-03 08:58:39 +0100

    Updating the branch with changes already in master

--- com.team6.project/src/main/java/com/team6/project/services/rest/UserManagmentRestService.java
+++ com.team6.project/src/main/java/com/team6/project/services/rest/UserManagmentRestService.java
@@ -1,37 +1,41 @@
 package com.team6.project.services.rest;
+
 import java.util.Collection;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import com.team6.project.entities.Response;
 import com.team6.project.entities.User;
-
 import com.team6.project.services.PersistenceServiceLocal;
 import com.team6.project.services.QueryServiceLocal;
 
 /**
- * Restful Service Class for managing the 
- * users of the system
+ * Restful Service Class for managing the users of the system
+ * 
  * @author Cristiana Conti
  *
  */
 @Path("/usermanagement")
 public class UserManagmentRestService {
-    
+
     @Inject
     private PersistenceServiceLocal persistence;
-    
+
     @Inject
     private QueryServiceLocal query;
-    
-    
+    @Context
+    private HttpServletResponse response;
+
     /**
      * 
      * @param userId
@@ -40,17 +44,17 @@ public class UserManagmentRestService {
     @GET
     @Path("/{userId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUserByKey(@PathParam("userId") String userId){
+    public User getUserByKey(@PathParam("userId") String userId) {
         return query.getUserByKey(userId);
     }
-    
+
     @GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<User> getAllUser(){
+    public Collection<User> getAllUser() {
         return query.getAllUser();
     }
-    
+
     /**
      * 
      * @param user
@@ -59,17 +63,28 @@ public class UserManagmentRestService {
     @POST
     @Path("/add")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response addUser(User user) {
-        return persistence.addUser(user);
+        if (query.getUserByKey(user.getKey()) == null) {
+            persistence.addUser(user);
+            String result = "User inserted with success";
+            return Response.status(Status.OK).entity(result).build();
+        }
+        String result = "User already into the system";
+        return Response.status(Status.BAD_REQUEST).entity(result).build();
     }
-    
+
     @POST
     @Path("/update")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response updateUser(User user) {
-        return persistence.updateUser(user);
+        if (query.getUserByKey(user.getKey()) != null) {
+            persistence.updateUser(user);
+            String result = "User updated with success";
+            return Response.status(Status.OK).entity(result).build();
+        } 
+        String result = "User not found";
+        return Response.status(Status.NOT_FOUND).entity(result).build();
+       
     }
 
 }
