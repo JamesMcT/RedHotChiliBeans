3a79099b2d4b5f9bdc3ecfaede6efa09e39fb0b2
Sabee <s.hutvagner@yahoo.com>
2015-04-13 13:47:54 +0100

    java doc part

--- com.team6.project/src/main/java/com/team6/project/services/QueryService.java
+++ com.team6.project/src/main/java/com/team6/project/services/QueryService.java
@@ -43,71 +43,37 @@ public class QueryService implements QueryServiceLocal {
 	public QueryService() {
 	}
 
-	
-	/**
-     * Finding all the user equipment
-     * 
-     * @return
-     */	
+		
 	@Override
 	public Collection<UserEquipment> getAllUserEquipment() {
 		return userEquipment.getAllUserEquipment();
 	}
 
 	
-	/**
-     * Finding all the event affected by the imsi
-     * 
-     * @param IMSI
-     * @return
-     */
 	@Override
 	public Collection<EventCause> findByIMSI(BigInteger IMSI) {
 		return baseData.findByImsi(IMSI);
 	}
 
-	
-	/**
-     * Finding user by id
-     * 
-     * @param newUserId
-     * @return
-     */
+		
 	@Override
 	public User getUserByKey(String newUserId) {
 		return user.getUserByKey(newUserId);
 	}
 
 	
-	/**
-     * Finding all the users
-     * 
-     * @return
-     */
 	@Override
 	public Collection<User> getAllUser() {
 		return user.getAllUser();
 	}
 
-	/**
-     * Finding all the event caused by tac (userequipment)
-     * 
-     * @param ue
-     * @return
-     */
+	
 	@Override
 	public Collection<Object[]> getDistinctEventByTac(Integer ue) {
 		return baseData.getDistinctEventByTac(ue);
 	}
 
-	
-	/**
-     * Counting failures and duration effected IMSI in the given time period
-     * 
-     * @param startDate
-     * @param endDate
-     * @return
-     */
+		
 	@Override
 	public Collection<Object[]> getFailureCountAndDurationPerImsiByDate(
 			Date startDate, Date endDate) {
@@ -122,36 +88,20 @@ public class QueryService implements QueryServiceLocal {
 //	}
 
 	
-	/**
-     * Counting failures by given IMSI in the given time period
-     * 
-     * @param imsi
-     * @param startDate
-     * @param endDate
-     * @return
-     */
+	
 	@Override
 	public Collection<BaseData> countCallFailurePerImsiByDate(BigInteger imsi, Date startDate, Date endDate) {
 		return baseData.countCallFailurePerImsiByDate(imsi, startDate, endDate);
 	}
 
 	
-	/**
-     * Finding all the imsis
-     * 
-     * @return
-     */
+	
 	public Collection<BigInteger> getAllImsi() {
 		return baseData.getAllImsi();
 	}
 
 	
-	/**
-     * Finding events caused by given IMSI
-     * 
-     * @param imsi
-     * @return
-     */
+	
 	@Override
 	public Collection<Object[]> getUniqueEventCauseByImsi(BigInteger imsi) {
 		return baseData.getUniqueEventCauseByImsi(imsi);
--- com.team6.project/src/main/java/com/team6/project/services/QueryServiceLocal.java
+++ com.team6.project/src/main/java/com/team6/project/services/QueryServiceLocal.java
@@ -23,39 +23,97 @@ import com.team6.project.entities.UserEquipment;
 public interface QueryServiceLocal {
 	
 	/**
-	 * 
-	 * @return
-	 */
+     * Finding all the user equipment
+     * 
+     * @return
+     */	
 	public Collection<UserEquipment> getAllUserEquipment();
 	
+	
+	/**
+     * Finding all the event affected by the imsi
+     * 
+     * @param IMSI
+     * @return
+     */
 	public Collection<EventCause> findByIMSI(BigInteger IMSI);
 
+	
+	/**
+     * Finding user by id
+     * 
+     * @param newUserId
+     * @return
+     */
 	public User getUserByKey(String newUserId);
 	
+	
+	/**
+     * Finding all the users
+     * 
+     * @return
+     */
 	public Collection<User> getAllUser();
 
 	
+	/**
+     * Finding all the event caused by tac (userequipment)
+     * 
+     * @param ue
+     * @return
+     */
 	public Collection<Object[]> getDistinctEventByTac(Integer ue);
+	
+	
 	
 	public long countCallFailureByTac(Integer tac, Date fromDate, Date toDate);
 	
 	public Collection<Object[]> getTOP10MarketOperatorCellByDate(Date fromDate, Date toDate);
 	
+	
+	/**
+     * Counting failures and duration effected IMSI in the given time period
+     * 
+     * @param startDate
+     * @param endDate
+     * @return
+     */
 	public Collection<Object[]> getFailureCountAndDurationPerImsiByDate(Date startDate, Date endDate);
 
 
 //	public Collection<BaseData> findImsiByDate(Date firstDate, Date secondDate);
 	
+	
+	/**
+     * Counting failures by given IMSI in the given time period
+     * 
+     * @param imsi
+     * @param startDate
+     * @param endDate
+     * @return
+     */
 	public Collection<BaseData> countCallFailurePerImsiByDate(BigInteger imsi, Date startDate, Date endDate);
 
 	public Collection<Object[]> findImsiByDate(Date firstDate, Date secondDate);
 
 
-	
+	/**
+     * Finding all the imsis
+     * 
+     * @return
+     */
 	public Collection<BigInteger> getAllImsi();
+	
+	
 	/**
      * @return All the unique EventCause objects and the 
      * number of their occurrences associated to a specific imsi. 
+     */
+	/**
+     * Finding events caused by given IMSI
+     * 
+     * @param imsi
+     * @return
      */
 	public Collection<Object[]> getUniqueEventCauseByImsi(BigInteger imsi);
 	public Collection<Object[]> getTopTenFailuresByDate(Date start, Date end);
